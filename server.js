7496b9bfd9e6494529b9ea51884373bc3f7bb324
asinn826 <alfredsin826@hotmail.com>
2018-06-28 00:03:45 -0700

    bring in css - attempt 1

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ var initDb = function(callback) {
   });
 };
 
+app.use(express.static('views'));
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
